e2934df723e0e71052d041be8ba00247d3efc569
ShantoKumarSarker <sk.bd2007@gmail.com>
2022-04-07 15:08:47 +0600

    new requirement implementation and fixes

--- src/components/blog-card/blog-card.component.jsx
+++ src/components/blog-card/blog-card.component.jsx
@@ -1,18 +1,16 @@
 import React from 'react'
 
-// COMPONENT
-import Button from '../button/button.component'
-
 import './blog-card.style.scss'
 
-export default function BlogCard({image, text}) {
+export default function BlogCard({image, text, href}) {
   return (
     <div className='blog'>
         <div className="blog__img-container">
             <img src={image} alt="image" />
         </div>
         <p className="blog__text">{text}</p>
-        <Button text='Read More'/>
+        {/* <Button text='Read More'/> */}
+        <a target="_blank" href={href}>Read More</a>
     </div>
   )
 }
